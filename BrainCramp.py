eec957b6ced67c1eec8f1ba2480a1966d84aad6b
Samuel Yvon <samuel@m3touchinc.com>
2017-03-23 19:21:20 -0400

    Worked on the optimisations, fixed the readme

--- BrainCramp.py
+++ BrainCramp.py
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (c) 2016 Samuel Yvon
+# Copyright (c) 2017 Samuel Yvon
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -22,7 +22,7 @@
 
 # This is a simple brainf*ck interpreter.
 # Samuel Yvon
-
+import sys
 from enum import Enum
 
 
@@ -35,13 +35,20 @@ class OpCode(Enum):
     WRITE = ".",
     LOOP_START = "[",
     LOOP_END = "]",
-    RESET_TO_ZERO = "RESET_TO_ZERO"
+    RESET_TO_ZERO = "RESET_TO_ZERO",
+    TRANSFER_VAL = "TRANSFER_VAL"
+
+
+class ValueTransferArg:
+    def __init__(self):
+        self.inc_ptrs = []
 
 
 class Instruction:
     def __init__(self, op_code, arg):
         self.code = op_code
         self.arg = arg
+        self.extra = None
 
 
 class BrainCramp:
@@ -53,6 +60,10 @@ class BrainCramp:
         self.memory = []
         self.memory_length = 0
         self.extend_memory()
+        # optimisation vars
+        self.move_right_val = 0
+        self.left_blocks = 0
+        self.can_op = True
 
     def write_arg(self):
         if len(self.ipt) > self.argReader:
@@ -119,12 +130,6 @@ class BrainCramp:
                 begin.append(ip)
         return jump_table
 
-    def optimize(self):
-        # This will execute until the current value is 0
-        self.code = self.code.replace("[-]", "F")
-        # same as previous instruction
-        self.code = self.code.replace("[+]", "F")
-
     def interpret(self):
         # self.optimize()
         ipt_len = len(self.code)
@@ -137,10 +142,23 @@ class BrainCramp:
                 latest = instruction_set[instruction_set_len - 1]
                 if latest.code == op_code and latest.arg is not None:
                     latest.arg += 1
+                    if op_code == OpCode.LEFT:
+                        self.move_right_val += 1
                 else:
                     instruction_set.append(Instruction(op_code, default_arg))
+
+                    if op_code == OpCode.PLUS:
+                        self.left_blocks += 1
+                    elif op_code == OpCode.LEFT:
+                        self.move_right_val += 1
+
             else:
                 instruction_set.append(Instruction(op_code, default_arg))
+
+                if op_code == OpCode.PLUS:
+                    self.left_blocks += 1
+                elif op_code == OpCode.LEFT:
+                    self.move_right_val += 1
 
         last_opening = -1
 
@@ -149,55 +167,115 @@ class BrainCramp:
 
             if c == ">":
                 incr_in_set(OpCode.RIGHT, 1)
+                self.last_right = len(instruction_set)
             elif c == "<":
                 incr_in_set(OpCode.LEFT, 1)
+                self.last_left = len(instruction_set)
             elif c == "+":
                 incr_in_set(OpCode.PLUS, 1)
             elif c == "-":
                 incr_in_set(OpCode.MINUS, 1)
             elif c == ".":
                 instruction_set.append(Instruction(OpCode.WRITE, None))
+                self.can_op = False
             elif c == ",":
                 instruction_set.append(Instruction(OpCode.READ, None))
+                self.can_op = False
             elif c == "[":
                 last_opening = len(instruction_set)
+                # we reset the numbers of right movements since we cancel the last optimisation
+                self.move_right_val = 0
+                self.can_op = True
+                self.left_blocks = 0
                 instruction_set.append(Instruction(OpCode.LOOP_START, None))
             elif c == "]":
 
                 could_optimize = False
 
-                diff = len(instruction_set) - last_opening
+                loop_block = instruction_set[last_opening + 1:]  # we do not want the loop start
+                diff = len(loop_block)
+                if diff > 1:
+                    if diff == 2:
+                        last_idx = len(instruction_set) - 1
+                        last_instr = instruction_set[last_idx]
 
-                if diff == 2:
-                    last_idx = len(instruction_set) - 1
-                    last_instr = instruction_set[last_idx]
+                        if last_instr.code == OpCode.PLUS or last_instr.code == OpCode.MINUS:
+                            del instruction_set[last_opening:]
+                            instruction_set.append(Instruction(OpCode.RESET_TO_ZERO, None))
+                            could_optimize = True
 
-                    if last_instr.code == OpCode.PLUS or last_instr.code == OpCode.MINUS:
-                        del instruction_set[last_opening:]
-                        instruction_set.append(Instruction(OpCode.RESET_TO_ZERO, None))
-                        could_optimize = True
+                        pass  # this is a reduce or increment to 0
+                    # ToDo : the line self.last_left > self.last_right is not really the trick here...
+                    # ToDo : we don't want sets of < or > that inter-cross each other, but we don't mind
+                    # ToDo : >>><<<< or <<<<>>>>
+                    elif self.can_op and self.last_left > self.last_right:
+                        """
+                        Detecting copy or multiplication sounds a little tricky...
+                        Structures like ->(x)+>(x)+<<
+                        or
+                                        ->(x)+(x)>(x)+(x)<<
 
-                    pass  # this is a reduce or increment to 0
-                else:
-                    """
-                    Need to optimize for copy or multiplicate
-                    """
-                    pass
+                        The general structure seems to be dec, move, inc, repeat until you move
+                        We need to move back (left) the same amount we moved right
+                        So that could be used to detect it.
+
+
+                        The minus can be at the end, like in the hello world example. In fact, it needs to be after or
+                        before the group of move left or right
+                        """
+
+                        left_blocks = []
+
+                        for instruc in loop_block:
+                            if instruc.code == OpCode.LEFT:
+                                left_blocks.append(instruc)
+                        if len(left_blocks) == 1:
+                            only_left = left_blocks[0]
+                            if only_left.arg == self.move_right_val:
+                                if loop_block[-1:][0].code == OpCode.MINUS or loop_block[:-1][0].code == OpCode.MINUS:
+                                    """
+                                    If we are here, it is because we detected a loop that can be turned into a transfer block
+
+                                    """
+                                    transfer_arg = ValueTransferArg()
+                                    minus_c = 0
+                                    ptr_diff = 0
+                                    for instruc in loop_block:
+                                        if instruc.code == OpCode.RIGHT:
+                                            ptr_diff += instruc.arg
+                                        elif instruc.code == OpCode.PLUS:
+                                            transfer_arg.inc_ptrs.append((ptr_diff, instruc.arg))
+                                        elif instruc.code == OpCode.MINUS:
+                                            # ToDo: if a minus is not first of last, it is because we reduce
+                                            # ToDo: instead of copying. Therefore we can also optimise that case
+                                            minus_c += 1
+
+                                    if minus_c < 2:
+                                        del instruction_set[last_opening:]
+                                        instr = Instruction(OpCode.TRANSFER_VAL, transfer_arg)
+                                        instr.extra = [str(c.code) + "{%d}" % c.arg for c in loop_block]
+                                        instruction_set.append(instr)
+                                        could_optimize = True
+
+                self.can_op = False
+                self.move_right_val = 0
+                self.left_blocks = 0
 
                 if not could_optimize:
                     instruction_set.append(Instruction(OpCode.LOOP_END, None))
-            else:
-                print("Unmapped: " + c)
 
             ip += 1
 
-        for c in instruction_set:
-            print("%s %s" % (c.code, str(c.arg)))
+        #for c in instruction_set:
+        #    if c.code == OpCode.TRANSFER_VAL:
+        #        print("Transfer to:")
+        #        print(c.extra)
+        #    else:
+        #        pass  # print("%s %s" % (c.code, str(c.arg)))
 
         return self.run(instruction_set)
 
     def run(self, instruction_set):
-        output = ""
         jump_table = self.build_jump_table(instruction_set)
         instruction_size = len(instruction_set)
         position = 0
@@ -224,17 +302,27 @@ class BrainCramp:
             elif c.code == OpCode.READ:
                 self.write_arg()
             elif c.code == OpCode.WRITE:
-                output += self.read_ascii()
+                new_letter = self.read_ascii()
+                sys.stdout.write(new_letter)
+                sys.stdout.flush()
             elif c.code == OpCode.RESET_TO_ZERO:
+                self.write(0)
+            elif c.code == OpCode.TRANSFER_VAL:
+                transfer_arg = c.arg
+                base = self.read()
+                for adrr_and_ratio in transfer_arg.inc_ptrs:
+                    addr = adrr_and_ratio[0] + self.pointer
+                    ratio = adrr_and_ratio[1]
+                    curr = self.read_at(addr)
+                    curr += (base * ratio)
+                    self.write_at(addr, curr)
                 self.write(0)
 
             position += 1
-        return str(output)
 
 
 br = BrainCramp(
-    "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.")
+    """++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.""")
 
 r = br.interpret()
-print(r)
 # Prints Hello world!
--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # BrainCramp
-A python brainfuck interpreter
+An optimized python brainfuck interpreter
 
-It is a very small brainf**k interpreter made to solve a codewars kata. It needed no speed optimisation beyond the basic decrement/increment cell to 0 ( [-] and [+])
+It performs the following optimisations:
+
+- [-] or [+] into a reset to zero
+- Builds a jump table instead of manually going back and find the loop start
+- Transforms some loops into copy to or copy and multiply, depending of the number of + (this is not completly done)
+
+### The plan is also to include a C JIT version of the braincramp interpreter
